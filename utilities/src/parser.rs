b95e2c1f5b2ec9ae6fc44d8bedb7a8fffa985a56
Jeff Mitchell <sentinel1909@jeff-mitchell.dev>
2024-02-12 21:37:50 -0800

    bug: partially pass day 2 tests

--- utilities/src/parser.rs
+++ utilities/src/parser.rs
@@ -5,7 +5,7 @@ use nom::bytes::complete::{take, tag};
 use nom::combinator::not;
 use nom::multi::many0;
 use nom::multi::many_till;
-use nom::sequence::{preceded, tuple};
+use nom::sequence::{preceded, terminated};
 use nom::IResult;
 
 // parser to look for the word "elf"
@@ -23,13 +23,17 @@ fn elf_on_a(input: &str) -> IResult<&str, &str> {
     tag("elf on a ")(input)
 }
 
+fn no_whitespace_elf_on_a(input: &str) -> IResult<&str, &str> {
+    tag("elf on a")(input)
+}
+
 // parser to look for the phrase "elf on a shelf"
 fn elf_on_a_shelf(input: &str) -> IResult<&str, &str> {
     preceded(elf_on_a, shelf)(input)
 }
 
-fn shelf_no_elf(input: &str) -> IResult<&str, ()> {
-    not(elf_on_a_shelf)(input)
+fn shelf_no_elf(input: &str) -> IResult<&str, &str> {
+    terminated(no_whitespace_elf_on_a, shelf)(input)
 }
 
 // function to parse the input string for instances of the word "elf"
@@ -43,8 +47,12 @@ pub fn parse_elf_on_a_shelf(input: &str) -> IResult<&str, Vec<(Vec<&str>, &str)>
 }
 
 // function to parse the input string for instances of the word "shelf"
-pub fn parse_shelves_with_no_elves(input: &str) -> IResult<&str, Vec<(Vec<&str>, ((), &str))>> {
-    many0(many_till(take(1usize), tuple((shelf_no_elf, shelf))))(input)
+pub fn parse_shelves(input: &str) -> IResult<&str, Vec<(Vec<&str>, &str)>> {
+    many0(many_till(take(1usize), shelf))(input)
+}
+
+pub fn parse_shelves_no_elves(input: &str) -> IResult<&str, Vec<(Vec<&str>, &str)>> {
+    many0(many_till(take(1usize), shelf_no_elf))(input)
 }
 
 #[cfg(test)]
@@ -53,7 +61,7 @@ mod tests {
 
     #[test]
     fn test_parse_elf() {
-        let input = "The mischievous elf peeked out from behind the toy workshop, and another elf joined in the festive dance. Look, there is also an elf on that shelf!";
+        let input = "elf elf elf on a shelf";
         let elves = parse_elf(input).unwrap();
         let elf_count = elves.1.len();
         assert_eq!(elf_count, 4);
@@ -61,17 +69,25 @@ mod tests {
 
     #[test]
     fn test_parse_elf_on_a_shelf() {
-        let input = "there is an elf on a shelf on an elf. there is also another shelf in Belfast.";
+        let input = "elf elf elf on a shelf";
         let elves_on_shelves = parse_elf_on_a_shelf(input).unwrap();
         let elf_on_shelf_count = elves_on_shelves.1.len();
         assert_eq!(elf_on_shelf_count, 1);
     }
 
     #[test]
-    fn test_parse_shelves_with_no_elves() {
-        let input = "there is an elf on a shelf on an elf. there is also another shelf in Belfast.";
-        let shelves_no_elves = parse_shelves_with_no_elves(input).unwrap();
+    fn test_parse_shelves() {
+        let input = "elf elf elf on a shelf";
+        let shelves = parse_shelves(input).unwrap();
+        let shelf_count = shelves.1.len();
+        assert_eq!(shelf_count, 1);
+    }
+
+    #[test]
+    fn test_parse_shelves_no_elves() {
+        let input = "In Belfast I heard an elf on a shelf on a shelf on a ";
+        let shelves_no_elves = parse_shelves_no_elves(input).unwrap();
         let shelf_no_elf_count = shelves_no_elves.1.len();
-        assert_eq!(shelf_no_elf_count, 1);
+        assert_eq!(shelf_no_elf_count, 0);
     }
 }
